f6e91e8005f1491dddcc829c9c330bb7107513da
carlosprofesena <charli.mora@misena.edu.co>
2022-08-04 09:03:05 -0500

    Primera versión del proyecto Academia. Vamos en el método store del controlador de recursos

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,38 @@
+<html>
+    <head>
+        {{--@yield nos permitirá llamar esta parte de la
+            plantilla en otras vistas--}}
+        <title>Academia - @yield('titulo')</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <a class="navbar-brand">
+                <img src= {{ asset('logo.png') }} alt="" width="50" height="50">
+            </a>
+            <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div id="my-nav" class="collapse navbar-collapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Crear curso <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Sobre nosotros <span class="sr-only">(current)</span></a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <br>
+        <br>
+        <br>
+        <div class="container">
+            <br>
+            @yield('contenido')
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    </body>
+</html>
---
+++ resources/views/otros/nosotros.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('titulo', 'nosotros')
+
+@section('contenido')
+
+<h3>Sobre nosotros</h3>
+
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis cum, veniam quae assumenda consequuntur necessitatibus harum odit modi, pariatur suscipit, incidunt autem culpa amet distinctio sint sapiente laboriosam similique! Eveniet.</p>
+<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sapiente commodi, ea, quibusdam vel iure impedit vero obcaecati vitae officia suscipit molestiae quaerat cumque reiciendis minus deserunt. Quaerat voluptates fugiat corporis!</p>
+
+@endsection
